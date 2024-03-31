a68e3524db8dab23748d34eafed371e783042a99
saichethanr <saichethan9019@gmail.com>
2024-03-31 16:03:43 +0530

    no of set bits all approch

--- bitmanipulation/noofsetbits.cpp
+++ bitmanipulation/noofsetbits.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h> 
 using namespace std;
 
-int noofsetbits(int n){
+int noofsetbits_approach1(int n){
    int cnt=0;
    while(n>1){
     if(n%2==1){
@@ -14,8 +14,28 @@ int noofsetbits(int n){
    }
    return cnt;
 }
+int noofsetbits_approach2(int n){
+    int cnt =0;
+    while(n>1){
+        if(n&1){
+            cnt++;
+        }
+        n=n>>1;
+    }
+    return cnt;
+}
+
+int noofsetbits_approach3(int n){
+    int cnt =0;
+    while(n!=0){
+        n = n&(n-1);
+        cnt++;
+    }
+    return cnt;
+}
+
 
 int main(){
-    cout << noofsetbits(13) << endl;
+    cout << noofsetbits_approach3(13) << endl;
     return 0;
 }
