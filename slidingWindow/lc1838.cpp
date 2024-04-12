7be814616e2a06b7864c2e669670ccd9fc7d5a0c
saichethanr <saichethan9019@gmail.com>
2024-04-12 14:52:29 +0530

    1838 solved

--- slidingWindow/lc1838.cpp
+++ slidingWindow/lc1838.cpp
@@ -25,3 +25,27 @@ using namespace std;
         }
         return maxfreq; 
     }
+
+    int maxFrequency_solution(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int l=0;
+        int s = k;
+        int maxfreq = 0;
+        int r =0;
+        long long int sum=0;
+        while(r<n){
+            sum+=nums[r];
+            long long int cost = (long long) nums[r] * (r-l+1)-sum;
+            while(cost>k){
+                sum-=nums[l];
+                l++;
+                cost=(long long) nums[r] * (r-l+1)-sum;
+            }
+
+            maxfreq = max(maxfreq,(r-l+1));
+            r++;
+           
+        }
+        return maxfreq; 
+    }
