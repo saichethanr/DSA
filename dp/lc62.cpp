9d94533ea857cdd92fc339c8acb75f37471d36eb
saichethanr <saichethan9019@gmail.com>
2024-04-13 16:09:01 +0530

    62 lc tab

--- dp/lc62.cpp
+++ dp/lc62.cpp
@@ -19,3 +19,27 @@ using namespace std;
           dp[i][j] = up+left;
           return dp[i][j];
     }
+
+        int uniquePaths_tab(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0){
+                        left =dp[i][j-1];
+                    }
+                    dp[i][j] = up+left;
+                }
+            }
+           
+        }
+       return dp[m-1][n-1];
+    } 
