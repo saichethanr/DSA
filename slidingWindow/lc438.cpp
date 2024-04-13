3fc1d2e57d79355bdf6344966f4b33c81e0ea1e8
saichethanr <saichethan9019@gmail.com>
2024-04-13 08:48:46 +0530

    find anagrams

--- slidingWindow/lc438.cpp
+++ slidingWindow/lc438.cpp
@@ -7,6 +7,7 @@ vector<int> findAnagrams(string s, string p) {
       int n=s.size();
       vector<int> ans;
       sort(p.begin(),p.end());
+      
       while(r<n){
            string str = s.substr(l,(r-l+1));
            sort(str.begin(),str.end());
@@ -15,6 +16,31 @@ vector<int> findAnagrams(string s, string p) {
            }
            r++;
            l++;
+      }
+
+      return ans;
+    }
+vector<int> findAnagrams(string s, string p) {
+      int l=0;
+      int r= 0;
+      int n=s.size();
+      vector<int>s_freq(26,0);
+      vector<int>p_freq(26,0);
+      vector<int> ans;
+      for(int i=0;i<p.size();i++){
+        p_freq[p[i]-'a']++;
+      }
+      while(r<n){
+          s_freq[s[r]-'a']++;
+          if((r-l+1)>p.size()){
+            s_freq[s[l]-'a']--;
+            l++;
+          }
+
+          if(s_freq==p_freq){
+            ans.push_back(l);
+          }
+          r++;
       }
 
       return ans;
