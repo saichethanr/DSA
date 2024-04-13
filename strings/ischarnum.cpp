e65cfff2b07b63b76ec9589bb047b2e052b4de40
saichethanr <saichethan9019@gmail.com>
2024-04-13 22:36:29 +0530

    testing

--- strings/ischarnum.cpp
+++ strings/ischarnum.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <cctype>
-
+using namespace std;
 int main() {
-    char c = 'a'; // character to check
-
+    char c = '7'; // character to check
+    int num = c-'0';
+    cout<<num<<endl;
     if (std::isdigit(c)) {
         std::cout << "The character is a number." << std::endl;
     } else {
