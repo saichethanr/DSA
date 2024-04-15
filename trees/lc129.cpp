d77a1cb8618ce7d11bcbdb891bb25026faa95405
saichethanr <saichethan9019@gmail.com>
2024-04-15 06:18:35 +0530

    sum root to leaf numbers tip

--- trees/lc129.cpp
+++ trees/lc129.cpp
@@ -11,6 +11,12 @@ using namespace std;
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
   };
  
+// remember what need to be done at every node do it first .. 
+// and then check if it can bbe done to left 
+// and check the same for right
+// and check for both left and right 
+
+
  void solve(TreeNode* root,string s,vector<string>&v){
         if(root==nullptr){
             return;
