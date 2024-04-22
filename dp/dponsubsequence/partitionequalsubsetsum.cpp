5a6cfd562e84a4f0749c8359f6e261b5defcccb4
saichethanr <saichethan9019@gmail.com>
2024-04-22 17:07:58 +0530

    partition equal subset sum

---
+++ dp/dponsubsequence/partitionequalsubsetsum.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h> 
+using namespace std;
+class Solution {
+public:
+    bool solve(int target,int n,vector<int>&nums,vector<vector<bool>>&dp){
+         for(int i=0;i<nums.size();i++){
+            dp[i][0] = true;
+         }
+         
+         dp[0][nums[0]]=true;
+
+         for(int i=1;i<nums.size();i++){
+            for(int j=1;j<=target;j++){
+                bool donttake = dp[i-1][j];
+                bool take = false;
+                if(j>=nums[i]){
+                    take = dp[i-1][j-nums[i]];
+                }
+                dp[i][j] = take | donttake;
+            }
+         }
+         return dp[n-1][target];
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum =0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        if(sum%2!=0){
+            return false; 
+        }
+        int n = nums.size();
+        vector<vector<bool>> dp(nums.size(),vector<bool>(sum,0));
+        int target = sum/2;
+        bool ans = solve(target,n,nums,dp);
+        return ans;
+    }
+};
