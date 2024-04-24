78b56e2fe23bd78b4592837df1d5cdf29c9c818e
saichethanr <saichethan9019@gmail.com>
2024-04-24 08:45:22 +0530

    question of 695

--- dfs-bfs/lc695.cpp
+++ dfs-bfs/lc695.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h> 
 using namespace std;
 
+
+// You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+// The area of an island is the number of cells with a value 1 in the island.
+
+// Return the maximum area of an island in grid. If there is no island, return 0.
+
+
 class Solution {
 public:
     void dfs(int i,int j,vector<vector<int>>& grid,int &cnt){
