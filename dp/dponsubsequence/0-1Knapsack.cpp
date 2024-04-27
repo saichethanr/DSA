1479300b5f2e7ad713774403a0071aea7cc48329
saichethanr <saichethan9019@gmail.com>
2024-04-27 13:23:23 +0530

    1-0 knapsack tabulation

--- dp/dponsubsequence/0-1Knapsack.cpp
+++ dp/dponsubsequence/0-1Knapsack.cpp
@@ -42,3 +42,24 @@ int solve_memo(int idx,int wt,int weight[],int  val[],vector<vector<int>>&dp){
         dp[idx][wt] = max(pick,nopick);
         return dp[idx][wt];
     }
+
+    int solve_tab(int wt,int n,int weight[],int  val[]){
+
+        vector<vector<int>>dp(n,vector<int>(wt+1,-1));
+        for(int i=weight[0];i<=wt;i++){
+                dp[0][i] = val[0];
+        }
+       
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=wt;j++){
+                int nopick = dp[i-1][j];
+                int pick = INT_MIN;
+                if(wt>=weight[i]){
+                    pick  = val[i] + dp[i-1][j-weight[i]];
+                }
+              dp[i][wt] = max(pick,nopick);
+            }
+        }
+    
+        return dp[n-1][wt];
+    }
