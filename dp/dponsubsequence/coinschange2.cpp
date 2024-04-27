7300e877f0982f385f25420fec1ef8dcbb1a3e34
saichethanr <saichethan9019@gmail.com>
2024-04-27 19:08:40 +0530

    coins change 2 rec + memo + tab done

---
+++ dp/dponsubsequence/coinschange2.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+
+//  simple recursion
+  int solve(int idx,int target,vector<int>& coins,vector<vector<int>> &dp){
+        
+        if(idx==0){
+            if(target%coins[idx]==0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        
+        
+        int notake = solve(idx-1,target,coins,dp);
+        int take =0;
+        if(target>=coins[idx]){
+            take = solve(idx,target-coins[idx],coins,dp);
+        }
+        return take + notake;
+    }
+
+
+//   memoization
+  int solve(int idx,int target,vector<int>& coins,vector<vector<int>> &dp){
+        
+        if(idx==0){
+            if(target%coins[idx]==0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        
+        if(dp[idx][target]!=-1){
+            return dp[idx][target];
+        }
+
+        int notake = solve(idx-1,target,coins,dp);
+        int take =0;
+        if(target>=coins[idx]){
+            take = solve(idx,target-coins[idx],coins,dp);
+        }
+        dp[idx][target] = take + notake;
+        return dp[idx][target];
+    }
+
+    //tabulation
+      int solve(int target,vector<int>& coins,vector<vector<int>> &dp){
+        
+        int n = coins.size();
+        for(int i=0;i<=target;i++){
+            if(i%coins[0]==0){
+                dp[0][i] = 1;
+            }
+            else{
+                dp[0][i]=0;
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=target;j++){
+               int notake = dp[i-1][j];
+               int take =0;
+                if(j>=coins[i]){
+                    take = dp[i][j-coins[i]];
+                }
+                dp[i][j] = take + notake;
+            }
+        }
+
+        
+        return dp[n-1][target];
+    }
