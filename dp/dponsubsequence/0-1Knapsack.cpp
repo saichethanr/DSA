fca42f261b27585037e13e28b109254220f1f762
saichethanr <saichethan9019@gmail.com>
2024-04-27 11:33:05 +0530

    0-1 knapsack memoization done

--- dp/dponsubsequence/0-1Knapsack.cpp
+++ dp/dponsubsequence/0-1Knapsack.cpp
@@ -10,8 +10,6 @@ int solve_rec(int idx,int wt,vector<int>&weight,vector<int>&val){
             return 0;
         }
     }
-
-
     int nopick = solve_rec(idx-1,wt,weight,val);
     int pick = INT_MIN;
     if(wt>=weight[idx]){
@@ -20,3 +18,27 @@ int solve_rec(int idx,int wt,vector<int>&weight,vector<int>&val){
 
     return max(pick,nopick);
 }
+
+int solve_memo(int idx,int wt,int weight[],int  val[],vector<vector<int>>&dp){
+        if(idx==0){
+            if(weight[0]<=wt){
+               return val[0];
+            }
+            else{
+                return 0;
+            }
+        }
+       
+         if(dp[idx][wt]!=-1){
+             return dp[idx][wt];
+         }
+        
+        int nopick = solve_memo(idx-1,wt,weight,val,dp);
+        int pick = INT_MIN;
+        if(wt>=weight[idx]){
+            pick  = val[idx] + solve_memo(idx-1,wt-weight[idx],weight,val,dp);
+        }
+    
+        dp[idx][wt] = max(pick,nopick);
+        return dp[idx][wt];
+    }
