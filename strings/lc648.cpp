de3435f1527c877f586e24276d96e3fc96beaa94
saichethanr <saichethan9019@gmail.com>
2024-04-30 14:12:12 +0530

    replace words question

--- strings/lc648.cpp
+++ strings/lc648.cpp
@@ -1,7 +1,13 @@
 #include<bits/stdc++.h> 
 #include <string>
 using namespace std;
-    
+
+// In English, we have a concept called root, which can be followed by some other word to form another longer word - let's call this word successor. For example, when the root "help" is followed by the successor word "ful", we can form a new word "helpful".
+
+// Given a dictionary consisting of many roots and a sentence consisting of words separated by spaces, replace all the successors in the sentence with the root forming it. If a successor can be replaced by more than one root, replace it with the root that has the shortest length.
+
+// Return the sentence after the replacement.
+
     class Solution {
     public:
         string replaceWords(vector<string>& dictionary, string sentence) {
