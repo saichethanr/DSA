f7cf00fa8768a827fbb65f5996d3472dfe3f216f
saichethanr <saichethan9019@gmail.com>
2024-05-04 20:09:23 +0530

    longets common substr memo

--- dp/dponstrings/longestcomsubstr.cpp
+++ dp/dponstrings/longestcomsubstr.cpp
@@ -13,3 +13,20 @@ using namespace std;
 
         return max(solve(ind1,ind2-1,s1,s2),solve(ind1-1,ind2,s1,s2));
     }
+
+    int solve(int ind1,int ind2,string s1,string s2,vector<vector<int>> &dp){
+        if(ind1<0 || ind2<0){
+            return 0;
+        }
+        if(dp[ind1][ind2]!=-1){
+            return dp[ind1][ind2];
+        }
+        if(s1[ind1]==s2[ind2]){
+            dp[ind1][ind2] = 1 + solve(ind1-1,ind2-1,s1,s2,dp);
+            return dp[ind1][ind2];
+        }
+
+        dp[ind1][ind2] = max(solve(ind1,ind2-1,s1,s2,dp),solve(ind1-1,ind2,s1,s2,dp));
+   
+        return dp[ind1][ind2];
+    }
