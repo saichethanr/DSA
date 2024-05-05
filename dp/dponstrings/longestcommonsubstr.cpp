59d349913c095a5c5efb74f1435dd716ac25dbe8
saichethanr <saichethan9019@gmail.com>
2024-05-05 12:24:56 +0530

    longest common substring

--- dp/dponstrings/longestcommonsubstr.cpp
+++ dp/dponstrings/longestcommonsubstr.cpp
@@ -1,3 +1,30 @@
  #include<bits/stdc++.h> 
 using namespace std;
 
+    
+    int longestCommonSubstr (string s1, string s2, int n, int m)
+    {
+        
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        for(int j=0;j<=m;j++){
+            dp[0][j] =0;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        
+        int len = 0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    len = max(dp[i][j],len);
+                }
+                else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return len;
+    }
