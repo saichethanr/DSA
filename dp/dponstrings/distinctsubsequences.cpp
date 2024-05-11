63b5ab282bd214d2bde5cb0edc5c91c815626c19
saichethanr <saichethan9019@gmail.com>
2024-05-11 13:45:40 +0530

    distinct subsequence done

--- dp/dponstrings/distinctsubsequences.cpp
+++ dp/dponstrings/distinctsubsequences.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 
-class Solution {
-public:
+
     int solve(int i,int j,string s,string t,vector<vector<int>>&dp){
         //string to be matched exausted count
         if(j<0){
@@ -30,4 +29,33 @@ public:
     }
 
 
-};
+        int solve(string s,string t){
+        int n1 = s.size();
+        int n2 = t.size();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+      
+        // base case
+        for(int i=0;i<=n1;i++){
+            dp[i][0] = 1;
+        }
+        // as we have already written for 0 0 we dont need to overwidte it so start from j=1 
+        for(int j=1;j<=n2;j++){
+            dp[0][j] =0;
+        }
+
+
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+ dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+      return dp[n1][n2];
+
+       
+    }
