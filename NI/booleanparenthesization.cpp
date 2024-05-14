4330cbae6e3c7908a1ff01dcc5f7c9fba577dfcf
saichethanr <saichethan9019@gmail.com>
2024-05-14 07:34:20 +0530

    NI questions added

---
+++ NI/booleanparenthesization.cpp
@@ -0,0 +1,13 @@
+// Given a boolean expression s of length n with following symbols.
+// Symbols
+//     'T' ---> true
+//     'F' ---> false
+// and following operators filled between symbols
+// Operators
+//     &   ---> boolean AND
+//     |   ---> boolean OR
+//     ^   ---> boolean XOR
+// Count the number of ways we can parenthesize the expression so that the value of expression evaluates to true.
+
+// Note: The answer can be large, so return it with modulo 1003
+
---
+++ NI/snakeandladderproblem.cpp
@@ -0,0 +1,7 @@
+// Given a 5x6 snakes and ladders board, find the minimum number of dice throws required to reach the destination or 
+// last cell (30th cell) from the source (1st cell).
+// You are given an integer N denoting the total number of snakes and ladders and an array arr[] of 2*N 
+// size where 2*i and (2*i + 1)th 
+// values denote the starting and ending point respectively of ith snake or ladder. 
+// The board looks like the following.
+// Note: Assume that you have complete control over the 6 sided dice. No ladder starts from 1st cell.
