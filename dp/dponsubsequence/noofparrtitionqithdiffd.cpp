92354cc1c81ec7ebd297d273e5dbb3cb422e1ab6
saichethanr <saichethan9019@gmail.com>
2024-05-16 10:05:12 +0530

    no of partition with diffrence d

--- dp/dponsubsequence/noofparrtitionqithdiffd.cpp
+++ dp/dponsubsequence/noofparrtitionqithdiffd.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h> 
 using namespace std;
+int mod = (int)(1e9 +7);
 
-int solve(int idx,int n,int target,vector<int>&nums){
+  int solve(int idx,int n,int target,vector<int>&nums){
+ 
    if(idx==0){
         if(target==0 && nums[0]==0){
             return 2;
@@ -20,25 +22,14 @@ int solve(int idx,int n,int target,vector<int>&nums){
    return take+notake;
 }
 
-int solve(int idx,int n,int target,vector<int>&nums,vector <vector<int>>dp){
-     if(idx==0){
-        if(target==0 && nums[0]==0){
-            return 2;
-        }
-        if(target==0 || nums[idx]==target){
-            return 1;
-        }
-        return 0;
-   }
-
-   int notake = solve(idx-1,n,target,nums);
-   int take = 0;
-   if(target>=nums[idx]){
-     take = solve(idx-1,n,target-nums[idx],nums);
-   }
-   return take+notake;
+int countPartitions(int n, int d, vector<int> &arr) {
+int totsum=0;
+for(auto &it : arr){
+    totsum+=it;
 }
-
-int main(){
-  
+if(totsum-d<0 || (totsum-d)%2!=0){
+    return false;
+}
+int target = totsum/2;
+return solve(n-1,n,target,arr);
 }
