1f62126b14bf52f44b844596abe5e24fcb1a105e
saichethanr <saichethan9019@gmail.com>
2024-05-16 07:18:20 +0530

    minfalling path sum

--- dp/dpongrids/minipumfallingsum.cpp
+++ dp/dpongrids/minipumfallingsum.cpp
@@ -1,7 +1,9 @@
 #include<bits/stdc++.h> 
 using namespace std;
 
-
+// Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+// A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. 
+// Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
  int solve_recursion(int i,int j,vector<vector<int>>& matrix){
         int n = matrix[0].size();
          if(j<0 || j>=n){
