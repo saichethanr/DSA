08439361c3ffc0eef1f929cb45a6d96dbd79d2bd
saichethanr <saichethan9019@gmail.com>
2024-05-16 08:17:07 +0530

    equla subset sum

--- dp/dponsubsequence/partitionequalsubsetsum.cpp
+++ dp/dponsubsequence/partitionequalsubsetsum.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h> 
 using namespace std;
+
+
+// Given an integer array nums, return true if you can partition the array into two subsets such that 
+// the sum of the elements in both subsets is equal or false otherwise.
+
+
 class Solution {
 public:
     bool solve(int target,int n,vector<int>&nums,vector<vector<bool>>&dp){
