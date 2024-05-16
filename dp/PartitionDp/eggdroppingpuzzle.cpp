c6147df25ce584eb51366a9762a85264ae211fd4
saichethanr <saichethan9019@gmail.com>
2024-05-16 09:16:41 +0530

    egg dropping puzzle

--- dp/PartitionDp/eggdroppingpuzzle.cpp
+++ dp/PartitionDp/eggdroppingpuzzle.cpp
@@ -33,3 +33,52 @@ class Solution
         return solve(n,k);
     }
 };
+
+
+
+class Solution
+{ 
+    public:
+    //Function to find minimum number of attempts needed in 
+    //order to find the critical floor.
+    int solve(int n,int k){
+        vector<vector<int>> dp(n+1,vector<int>(k+1,0));
+        int mini = INT_MAX;
+        int res;
+        int x;
+      
+        
+        
+        for(int i=1;i<=n;i++){
+            dp[i][1] = 1;
+            dp[i][0] = 0;
+        }
+        
+        for(int i=1;i<=k;i++){
+            dp[1][i] = i;
+        }
+        
+        for(int i=2;i<=n;i++){
+            for(int j=2;j<=k;j++){
+                dp[i][j] =INT_MAX;
+                  for(int x=1;n<=k;x++){
+                      
+                        res = max(dp[i-1][j-1],dp[i][j-x]);
+                            if(res<dp[i][j]){
+                                dp[i][j] = res;
+                            }
+                }
+                
+                
+        
+            }
+        }
+        
+        return dp[n][k];
+        
+    }
+    int eggDrop(int n, int k) 
+    {
+        return solve(n,k);
+    }
+};
