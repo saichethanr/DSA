e750ed5b32c5ad68c5a16f6f45dc211b7cd1dd90
saichethanr <saichethan9019@gmail.com>
2024-05-19 22:48:23 +0530

    subsequence recursion

--- recursion/subsequencewithsumk.cpp
+++ recursion/subsequencewithsumk.cpp
@@ -28,9 +28,62 @@ void solve(int i,vector<int>nums,vector<int> t,int sum,int target){
 
 }
 
+
+//this is for prining just one subsequence 
+bool f(int i,vector<int>nums,vector<int> t,int sum,int target){
+    if(i==nums.size()){
+        if(sum==target){
+            print(t);
+            return true;
+        }
+        return false;
+    }
+    
+    //take case
+    t.push_back(nums[i]);
+    sum+=nums[i];
+    //if the condition satifies if u dont want to call further recursive calls
+    if(f(i+1,nums,t,sum,target) == true){
+        return true;
+    }
+    //notake case
+    t.pop_back();
+    sum-=nums[i];
+    if(f(i+1,nums,t,sum,target)==true){
+        return true;
+    }
+  return false;
+}
+//this is to count the no of subsequence
+int count(int i,vector<int>nums,vector<int> t,int sum,int target){
+    if(i==nums.size()){
+        if(sum==target){
+            print(t);
+            return 1;
+        }
+        return 0;
+    }
+    
+    //take case
+    t.push_back(nums[i]);
+    sum+=nums[i];
+    int take = count(i+1,nums,t,sum,target);
+
+    //notake case
+    t.pop_back();
+    sum-=nums[i];
+    int notake = count(i+1,nums,t,sum,target);
+    //in such questions where we would need the count just add dup the take and no take case and return
+    return take+notake;
+
+}
+
+
+
 int main(){
     vector <int> t;
     vector<int> nums = {1,2,1};
     int target = 2;
-    solve(0,nums,t,0,target);
+    int  ans = count(0,nums,t,0,target);
+    cout<<ans<<endl;
 }
