08dd2d7d7a694497ed63334cdba64ed11566c737
saichethanr <saichethan9019@gmail.com>
2024-05-22 17:49:32 +0530

    binary autocomplete trie

--- zscalar/binaryautocomplete.cpp
+++ zscalar/binaryautocomplete.cpp
@@ -1,29 +1,61 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-vector<int> solve(vector<string> vec) {
-    map<pair<int, int>, int> um;
-    vector<int> res;
-
-    for (int i = 0; i < vec.size(); i++) {
-        int a = 0;
-        int index = i;
-        for (int j = 0; j < vec[i].length(); j++) {
-            a = a * 2 + vec[i][j] - '0';
-            if (um.find({a, j + 1}) != um.end())
-                index = um[{a, j + 1}] + 1;
-            um[{a, j + 1}] = i;
+class Trie{
+    public:
+        map<int,int> hot;
+        map<int,unordered_map<int,int>> trie;
+        map<int,int> index;
+        int curr =0;
+        void insert(string& s,int ind){
+            int p=0;
+            for(int i=0;i<s.size();i++){
+                int temp = s[i]-'0';
+                if(trie.count(p)==0 || trie[p].count(temp)==0){
+                    trie[p][temp] = ++curr;
+                }
+                p = trie[p][temp];
+                index[p] = ind;
+            }
+            hot[p]++;
         }
-        res.push_back(index);
+        int search(string& s){
+            int p =0;
+            for(int i=0;i<s.size();i++){
+                int temp = s[i]-'0';
+                if(trie.count(p)==0 || trie[p].count(temp)==0) return false;
+                p = trie[p][temp];
+            }
+            return hot[p];
+        }
+        bool pref(string& s){
+            int p =0;
+            for(int i=0;i<s.size();i++){
+                int temp = s[i]-'0';
+                if(trie.count(p)==0 || trie[p].count(temp)==0) return false;
+                p = trie[p][temp];
+            }
+            return true;
+        }
+        int longest_pref(string& s,int ind){
+            int p =0;
+            int last_ind = -1;
+            for(int i=0;i<s.size();i++){
+                if(trie.count(p)==0 || trie[p].count(s[i]-'0')==0) break;
+                p = trie[p][s[i]-'0'];
+                last_ind = index[p];
+            }
+            insert(s,ind);
+            return last_ind;
+        }
+};
+int main(){
+    vector<string> arr = {"000","1110","01","001","110","11"};
+    Trie* t = new Trie();
+    for(int i=0;i<arr.size();i++){
+        int val = t->longest_pref(arr[i],i);
+        if(val==-1 && i==0) cout<<0<<" ";
+        else if(val==-1) cout<<i<<" ";
+        else cout<<val+1<<" ";
     }
-    
-    return res;
-}
-
-int main() {
-    vector<string> vec = {"000", "1110", "01", "001", "110", "11"};
-    vector<int> res = solve(vec);
-    for (auto i : res)
-        cout << i << " ";
-    cout << endl;
+    return 0;
 }
