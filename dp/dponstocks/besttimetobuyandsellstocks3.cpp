ce45a027095f01d1f154c8ca38f93f7434f27e66
saichethanr <saichethan9019@gmail.com>
2024-05-28 17:29:54 +0530

    best time to buy and sell stocks 3

--- dp/dponstocks/besttimetobuyandsellstocks3.cpp
+++ dp/dponstocks/besttimetobuyandsellstocks3.cpp
@@ -70,3 +70,50 @@ public:
         return ans;
     }
 };
+
+
+//tabulation code 
+
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+                
+
+        for(int ind=0;ind<prices.size();ind++){
+             for(int buy=0;buy<=1;buy++){
+                dp[ind][buy][0] = 0;
+             }
+        }
+
+        for(int buy=0;buy<=1;buy++){
+            for(int cap=0;cap<3;cap++){
+                dp[n][buy][cap] = 0;
+            }
+        }
+        
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<3;cap++){
+                     int profit=0;
+                        if(buy){
+                            //if u can buy u should loose amount 
+                        profit = max(-prices[ind]+dp[ind+1][0][cap],dp[ind+1][1][cap]);
+
+                        }
+                        else{
+                            //if u sell u can gain amount
+                            profit = max(prices[ind]+dp[ind+1][1][cap-1],dp[ind+1][0][cap]);
+                        }
+                        dp[ind][buy][cap] = profit;
+                }
+            }
+        }
+      
+        
+        return dp[0][1][2];
+    }
+};
