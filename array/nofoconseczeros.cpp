afc288d9cbfc7ab4c5dd0dd92cd4f714aec21380
saichethanr <saichethan9019@gmail.com>
2024-08-18 19:29:37 +0530

    answer for the question updated

--- array/nofoconseczeros.cpp
+++ array/nofoconseczeros.cpp
@@ -37,3 +37,51 @@ public:
         return res;
     }
 };
+
+
+
+
+
+//answer
+class Solution {
+public:
+//initial solution
+    long long zeroFilledSubarray(vector<int>& nums) {
+        map<long long  ,long long> mpp;
+        int r=0;
+        int n = nums.size();
+        int l=-1;
+        int cnt=0;
+        int z=0;
+        while(r<n){
+          if(nums[r]==0){
+             if(l==-1){
+                 l=r;
+             }
+            
+          }
+          else{
+            if(l!=-1){
+               long long x = r-l;
+               mpp[x]+=(x*(x+1))/2;
+            }
+            l=-1;
+          }
+          r++;
+        }
+        
+         if(l!=-1){
+               long long  x = r-l;
+               mpp[x]+=(x*(x+1))/2;
+        }
+
+         
+        long long int  res=0;
+        for(auto it : mpp){
+            cout<<it.first<<" "<<it.second<<endl;
+            res+=it.second;
+        }
+
+        return res;
+    }
+};
